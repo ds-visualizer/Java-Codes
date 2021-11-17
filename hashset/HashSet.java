49ddc03d6f76e1b6fb50576d311d353e6cb2f015
satvik-1203 <satvikgsu@gmail.com>
2021-11-16 19:07:20 -0500

    added hashset:

---
+++ hashset/HashSet.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class HashSet<T> {
+
+  class Entry {
+    T key;
+
+    Entry(T key) {
+      this.key = key;
+    }
+
+  }
+
+  private LinkedList<Entry>[] set = new LinkedList[10];
+
+  private int hash(T key) {
+
+    return key.hashCode() % set.length;
+  }
+
+  private Entry getEntry(T key, LinkedList<Entry> entries) {
+
+    for (Entry entry : entries) {
+      if (entry.key.equals(key))
+        return entry;
+    }
+
+    return null;
+  }
+
+  void add(T key) {
+    int hash = hash(key);
+    LinkedList<Entry> entries = set[hash];
+
+    if (entries == null) {
+
+      entries = new LinkedList<Entry>();
+      set[hash] = entries;
+
+    }
+
+    Entry entry = getEntry(key, entries);
+
+    if (entry != null)
+      return;
+
+    entries.add(new Entry(key));
+  }
+
+  void remove(T key) {
+    int hash = hash(key);
+    LinkedList<Entry> entries = set[hash];
+
+    if (entries == null)
+      return;
+
+    Entry entry = getEntry(key, entries);
+
+    if (entry == null)
+      return;
+
+    entries.remove(entry);
+  }
+
+  boolean has(T key) {
+    int hash = hash(key);
+    LinkedList<Entry> entries = set[hash];
+
+    if (entries == null)
+      return false;
+
+    Entry entry = getEntry(key, entries);
+
+    if (entry == null)
+      return false;
+
+    return true;
+  }
+
+}
